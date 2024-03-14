3039338d9b854d9cb48e4c780fc010bc9608fc7c
marina0088 <marina.isobe@gmail.com>
2024-03-14 16:19:13 +0900

    step1

---
+++ quest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "パスワードマネージャーへようこそ!"
+echo "サービス名を入力してください："
+read service_name
+echo "ユーザー名を入力してください："
+read username
+echo "パスワードを入力してください："
+read -s password
+echo "$service_name:$username:$password" >> pass.txt
+echo "Thank you!"
+
